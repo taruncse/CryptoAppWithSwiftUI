4354e039214557b568281649976ed900836ac90b
tarun.biswas <tarun.biswas@dreamonline.co.jp>
2022-09-01 00:44:58 +0600

    Initial Commit

---
+++ CryptoAppWithSwiftUI/CryptoAppWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppWithSwiftUIApp.swift
+//  CryptoAppWithSwiftUI
+//
+//  Created by DreamOnline on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
